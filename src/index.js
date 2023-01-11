9c50ecf48b4ebccc9cd6a83e93de3a6ef3c3b4ae
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2023-01-10 23:16:10 -0500

    stuck
    I don't know why it's not getting the API data. Will come bac to it.

--- src/index.js
+++ src/index.js
@@ -9,66 +9,32 @@ let day4 = days[(now.getDay() + 4) % 7];
 let day5 = days[(now.getDay() + 5) % 7];
 let day6 = days[(now.getDay() + 6) % 7];
 
-//the index no of today's date= now.getDay()
-//the index no of a day in the array=findIndex(
-
-let thingIAmDisplaying = document.querySelector(".weekly-forecast-grid");
-
-/*days.forEach(function (day) {
-  alert(day);
-});*/
-
 let geoUrl = "http://api.openweathermap.org/geo/1.0/direct";
 let oneCallUrl = "https://api.openweathermap.org/data/3.0/onecall";
 let apiAirQualUrl = "https://api.openweathermap.org/data/2.5/air_pollution";
 let apiKey = "8f909eb8beff1d1a0ae8b2df17dab17d";
 let units = "metric";
+
+//dehli
 lat = 28;
 lon = 77;
 
-/*let geoData;
-let oneCallData;
-let AirQualData;
-
-async function getGeoData() {
-  const response = await fetch(geoUrl);
-  geoData = await response.json();
-  console.log(geoData);
-}
-getGeoData();
-async function getOneCallData() {
-  const response = await fetch(oneCallUrl);
-  oneCallData = await response.json();
-  console.log(oneCallData);
-}
-getOneCallData();
-async function getAirQualData() {
-  const response = await fetch(apiAirQualUrl);
-  airQualData = await response.json();
-  console.log(airQualData);
-}
-getAirQualData();*/
-
-function createThings(response) {
+function createForecastGrid(response) {
+  console.log(response.data.daily[0].weather.description);
   let dailyData = response.data.daily;
   let forecastHTML = "";
-
+  let forecastGrid = document.querySelector(".weekly-forecast-grid");
   for (let i = 0; i < 7; i++) {
-    let forecastIcon = `<img class="forecast-icon" img src="https://openweathermap.org/img/wn/${response.data.daily[i].weather.icon}@2x.png"/>`;
+    let forecastIcon2 = dailyData[i].weather.icon;
     let temp = dailyData[i].temp.day;
     forecastHTML += `<div class="grid${i}">${eval(
       "day" + i
-    )}</div></br><div class="gridResponse">${temp}</div></br><div>${forecastIcon}</div>`;
+    )}</div></br><div class="gridResponse">${temp}</div></br><img class="forecast-icon" src="https://openweathermap.org/img/wn/${forecastIcon2}@2x.png" alt="forecast icon"/>`;
   }
-  thingIAmDisplaying.innerHTML = forecastHTML;
+  forecastGrid.innerHTML = forecastHTML;
 }
 axios
   .get(
     `${oneCallUrl}?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=${units}`
   )
-  .then(createThings);
-
-/*forecastIcon.setAttribute(
-      "alt",
-      `${response.data.daily[i].weather.description}`
-    );*/
+  .then(createForecastGrid);
